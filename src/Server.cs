679ead947f2d145cd3126496085685a9a2dccaaa
Prakhar Gupta <guptaprakhar@microsoft.com>
2025-10-18 16:12:13 +0530

    sending a default string response to client request

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,18 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 server.AcceptSocket(); // wait for client
+
+while (true)
+{
+    Socket client = server.AcceptSocket();
+
+    string response = "+PONG\r\n";
+    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+    client.Send(responseBytes);
+
+    client.Close();
+}
