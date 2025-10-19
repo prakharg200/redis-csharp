c50061634cfa832efac6186f0d5080759aa13d9a
Prakhar Gupta <guptaprakhar@microsoft.com>
2025-10-19 12:52:58 +0530

    adding support for ECHO command

--- src/Server.cs
+++ src/Server.cs
@@ -22,10 +22,77 @@ static async Task HandleClientAsync(Socket client)
         {
             break;
         }
-        string response = "+PONG\r\n";
+
+        string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        string response = ProcessCommand(request);
+
         byte[] responseBytes = Encoding.UTF8.GetBytes(response);
         await client.SendAsync(responseBytes, SocketFlags.None);
     }
 
     client.Close();
+}
+
+static string ProcessCommand(string request)
+{
+    var parts = ParseRESPArray(request);
+    if (parts.Count == 0)
+    {
+        return "-ERR invalid command\r\n";
+    }
+    string command = parts[0].ToUpperInvariant();
+    
+    if (command == "PING")
+    {
+        return "+PONG\r\n";
+    }
+    else if (command == "ECHO")
+    {
+        if (parts.Count < 2)
+        {
+            return "-ERR wrong number of arguments for 'ECHO' command\r\n";
+        }
+        return EncodeBulkString(parts[1]);
+    }
+
+    return "-ERR unknown command\r\n";
+}
+
+static List<string> ParseRESPArray(string request)
+{
+    var result = new List<string>();
+    var lines = request.Split("\r\n");
+
+    int i = 0;
+
+    if (i >= lines.Length || !lines[i].StartsWith('*'))
+    {
+        return result;
+    }
+
+    i++;
+
+    while (i < lines.Length - 1)
+    {
+        if (lines[i].StartsWith('$'))
+        {
+            i++;
+            if (i < lines.Length)
+            {
+                result.Add(lines[i]);
+                i++;
+            }
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return result;
+}
+
+static string EncodeBulkString(string value)
+{
+    return $"${value.Length}\r\n{value}\r\n";
 }
